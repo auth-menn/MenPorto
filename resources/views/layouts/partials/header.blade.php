310505e6c6adf322aee28b5c30c03637c5f9d327
auth-menn <auth.menn@gmail.com>
2025-07-03 14:36:56 +0700

    update

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -4,7 +4,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ config('app.name', 'Portfolio') }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!-- Font Awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     </head>
     <body class="bg-gradient-to-r from-gray-900 via-blue-900 to-gray-900">
